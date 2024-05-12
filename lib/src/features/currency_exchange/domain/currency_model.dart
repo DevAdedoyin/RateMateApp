9f000f8d2594b0f9d9f12d7a69dd0088bbbd7d86
Adedoyin <isradeoluwa@gmail.com>
2024-05-12 01:14:04 +0100

    Currency conversion models created

--- lib/src/features/currency_exchange/domain/currency_model.dart
+++ lib/src/features/currency_exchange/domain/currency_model.dart
@@ -1,5 +1,25 @@
+class CurrencyConversionModel {
+  final String time_last_update_utc;
+  final String time_next_update_utc;
+  final String base_code;
+  final Map<String, dynamic> conversionRates;
 
+  CurrencyConversionModel(
+      {required this.base_code,
+      required this.conversionRates,
+      required this.time_last_update_utc,
+      required this.time_next_update_utc});
 
-class CurrencyModel {
-  
+  factory CurrencyConversionModel.fromJson(Map<String, dynamic> data) {
+    final time_last_update_utc = data["time_last_update_utc"];
+    final time_next_update_utc = data["time_next_update_utc"];
+    final base_code = data["base_code"];
+    final conversionRates = data["conversion_rates"];
+
+    return CurrencyConversionModel(
+        base_code: base_code,
+        conversionRates: conversionRates,
+        time_last_update_utc: time_last_update_utc,
+        time_next_update_utc: time_next_update_utc);
+  }
 }
